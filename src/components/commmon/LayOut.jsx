11275002506a527ff5d1b600ce16b2ffd9ea10d6
Kimtaeyeon04 <te4939@naver.com>
2025-03-17 18:12:09 +0900

    footer 수정

--- src/components/commmon/LayOut.jsx
+++ src/components/commmon/LayOut.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header.jsx";
-import Footer from "./footer.jsx";
+import Footer from "./Footer.jsx";
 import styled from "styled-components";
 
 const LayOut = () => {
